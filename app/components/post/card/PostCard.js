92c51baf4e63ce566ca3bf8acad250f67ba2eb33
thangved <thangcreation@gmail.com>
2022-03-16 10:23:55 +0700

    update card

--- app/components/post/card/PostCard.js
+++ app/components/post/card/PostCard.js
@@ -1,69 +1,94 @@
-import { Avatar, Badge, Box, Heading, HStack, Text } from "@chakra-ui/react";
+import {
+	Avatar,
+	Badge,
+	Box,
+	Heading,
+	HStack,
+	Image,
+	Text,
+} from "@chakra-ui/react";
 import moment from "moment";
 import Link from "next/link";
 import { useRef } from "react";
 import Markdown from "../../markdown/Markdown";
 import PostTools from "../tools/PostTools";
-import styles from './PostCard.module.scss';
+import styles from "./PostCard.module.scss";
+
+/**
+ *
+ * @param {string} markdown
+ */
+const getImage = (markdown) => {
+	const mImg = markdown.match(/!\[.+\]\(.+\)/);
+	const sImg = mImg ? mImg[0] : "";
+
+	return sImg.replace(/!\[.+\]\(/, "").replace(")", "");
+};
 
 export default function PostCard({ post, highlight, disabled }) {
-    const refPost = useRef()
+	const refPost = useRef();
+	const imageLink = getImage(post.content);
 
-    return <Box
-        className={styles.wrapper}
-        ref={refPost}>
-        <HStack marginBottom="4">
-            <Avatar
-                name={post.author.fullName}
-                size="md" />
-            <div style={{
-                flex: 1
-            }}>
-                <Heading size="sm">
-                    {post.author.fullName}
-                </Heading>
-                <Text textAlign="left">
-                    {post.author.username}
-                </Text>
-            </div>
+	const thumbnail = imageLink ? (
+		<Image
+			borderRadius="5px"
+			width="100%"
+			src={imageLink}
+			alt={post.title}
+		/>
+	) : (
+		<Markdown>{`${post.content.slice(0, 200)}...`}</Markdown>
+	);
 
-            <PostTools
-                post={post}
-                onRemove={() => refPost.current?.remove()}
-            />
+	return (
+		<Box className={styles.wrapper} ref={refPost}>
+			<HStack marginBottom="4">
+				<Avatar name={post.author.fullName} size="md" />
+				<div
+					style={{
+						flex: 1,
+					}}
+				>
+					<Heading size="sm">{post.author.fullName}</Heading>
+					<Text textAlign="left">{post.author.username}</Text>
+				</div>
 
-        </HStack>
-        <Heading size="sm" marginBottom="2">
-            {post.title}
-        </Heading>
-        <Text>
-            Tạo vào {moment(post.createdAt).calendar()} (Sửa đổi lần cuối {moment(post.updatedAt).fromNow()})
-        </Text>
+				<PostTools
+					post={post}
+					onRemove={() => refPost.current?.remove()}
+				/>
+			</HStack>
+			<Heading size="sm" marginBottom="2">
+				{post.title}
+			</Heading>
+			<Text>
+				Tạo vào {moment(post.createdAt).calendar()} (Sửa đổi lần cuối{" "}
+				{moment(post.updatedAt).fromNow()})
+			</Text>
 
-        <HStack padding="10px 0" wrap="wrap">
-            {
-                post.topics.map(topic => (<Link key={topic} href={`/topic/${topic}`}>
-                    <a>
-                        <Badge
-                            colorScheme="green"
-                            variant={highlight === topic ? 'outline' : 'subtle'}>
-                            {topic}
-                        </Badge>
-                    </a>
-                </Link>))
-            }
-        </HStack>
-        {
-            disabled ? <Markdown>
-                {`${post.content.slice(0, 200)}...`}
-            </Markdown> : <Link href={`/post/${post.slug}`}>
-                <a>
-                    <Markdown>
-                        {`${post.content.slice(0, 200)}...`}
-                    </Markdown>
-                </a>
-            </Link>
-        }
-
-    </Box>
+			<HStack padding="10px 0" wrap="wrap">
+				{post.topics.map((topic) => (
+					<Link key={topic} href={`/topic/${topic}`}>
+						<a>
+							<Badge
+								colorScheme="green"
+								variant={
+									highlight === topic ? "outline" : "subtle"
+								}
+							>
+								{topic}
+							</Badge>
+						</a>
+					</Link>
+				))}
+			</HStack>
+			{disabled ? (
+				thumbnail
+			) : (
+				<Link href={`/post/${post.slug}`}>
+					<a>{thumbnail}</a>
+				</Link>
+			)}
+		</Box>
+	);
 }
--- app/pages/post/[slug]/index.js
+++ app/pages/post/[slug]/index.js
@@ -1,126 +1,117 @@
-import { Avatar, Badge, Box, Container, Divider, Flex, Heading, HStack, Spacer, Text, VStack } from "@chakra-ui/react";
+import {
+	Avatar,
+	Badge,
+	Box,
+	Container,
+	Divider,
+	Flex,
+	Heading,
+	HStack,
+	Spacer,
+	Text,
+	VStack,
+} from "@chakra-ui/react";
 import axios from "axios";
 import moment from "moment";
-import 'moment/locale/vi';
+import "moment/locale/vi";
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { toast } from 'react-hot-toast';
-import Markdown from '../../../components/markdown/Markdown';
-import PostCard from '../../../components/post/card/PostCard';
-import PostSkeletons from '../../../components/post/skeletons/PostSkeletons';
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import Markdown from "../../../components/markdown/Markdown";
+import PostCard from "../../../components/post/card/PostCard";
+import PostSkeletons from "../../../components/post/skeletons/PostSkeletons";
 import PostTools from "../../../components/post/tools/PostTools";
-import constants from '../../../config/constants';
-
+import constants from "../../../config/constants";
 
 export async function getServerSideProps(context) {
-    try {
-        const { slug } = context.query
+	try {
+		const { slug } = context.query;
 
-        const data = (await axios.get(`${constants.api}/post/get/${slug}`)).data.data
+		const data = (await axios.get(`${constants.api}/post/get/${slug}`)).data
+			.data;
 
-        return {
-            props: {
-                post: data || {}
-            },
-        }
-    }
-    catch (error) {
-        return {
-            props: {
-                post: {}
-            }
-        }
-    }
+		return {
+			props: {
+				post: data || {},
+			},
+		};
+	} catch (error) {
+		return {
+			props: {
+				post: {},
+			},
+		};
+	}
 }
 
 export default function Post({ post }) {
-    const [loading, setLoading] = useState(true)
-    const [posts, setPosts] = useState([])
-    const router = useRouter()
+	const [loading, setLoading] = useState(true);
+	const [posts, setPosts] = useState([]);
+	const router = useRouter();
 
-    useEffect(() => {
-        axios.get(`${constants.api}/post/user/${post.author?.username}`)
-            .then(({ data }) => {
-                if (!data.success)
-                    return toast.error(data.message)
+	useEffect(() => {
+		axios
+			.get(`${constants.api}/post/user/${post.author?.username}`)
+			.then(({ data }) => {
+				if (!data.success) return toast.error(data.message);
 
-                setPosts(data.data)
-                setLoading(false)
-            })
-            .catch(error => toast.error(error.toString()))
-    }, [post.author?.username])
-    return <Container maxW="container.md">
-        <Head>
-            <title>
-                {post.title}
-            </title>
-        </Head>
+				setPosts(data.data);
+				setLoading(false);
+			})
+			.catch((error) => toast.error(error.toString()));
+	}, [post.author?.username]);
+	return (
+		<Container maxW="container.md">
+			<Head>
+				<title>{post.title}</title>
+			</Head>
 
-        <Flex mt="2">
-            <Heading flex="1">
-                {post.title}
-            </Heading>
-            <PostTools
-                post={post}
-                onRemove={() => {
-                    router.back()
-                }}
-            />
-        </Flex>
+			<Flex mt="2">
+				<Heading flex="1">{post.title}</Heading>
+				<PostTools
+					post={post}
+					onRemove={() => {
+						router.back();
+					}}
+				/>
+			</Flex>
 
-        <Text>
-            Sửa đổi lần cuối {moment(post.updatedAt).fromNow()}
-        </Text>
+			<Text>Tạo vào {moment(post.createdAt).calendar()}</Text>
+			<Text>Sửa đổi lần cuối {moment(post.updatedAt).fromNow()}</Text>
 
-        <HStack wrap="wrap">
-            {post.topics?.map(topic => (<Link
-                href={`/topic/${topic}`}
-                key={topic}>
-                <a>
-                    <Badge
-                        colorScheme="green"
-                    >
-                        {topic}
-                    </Badge>
-                </a>
-            </Link>))}
-        </HStack>
+			<HStack wrap="wrap">
+				{post.topics?.map((topic) => (
+					<Link href={`/topic/${topic}`} key={topic}>
+						<a>
+							<Badge colorScheme="green">{topic}</Badge>
+						</a>
+					</Link>
+				))}
+			</HStack>
 
-        <Flex padding="20px 0">
-            <Avatar name={post.author?.fullName} />
-            <Box ml="3">
-                <Text>
-                    {post.author?.fullName}
-                </Text>
-                <Text>
-                    @{post.author?.username}
-                </Text>
-            </Box>
-        </Flex>
+			<Flex padding="20px 0">
+				<Avatar name={post.author?.fullName} />
+				<Box ml="3">
+					<Text>{post.author?.fullName}</Text>
+					<Text>@{post.author?.username}</Text>
+				</Box>
+			</Flex>
 
-        <Markdown>
-            {post.content}
-        </Markdown>
+			<Markdown>{post.content}</Markdown>
 
-        <Spacer height={20} />
+			<Spacer height={20} />
 
-        <Heading>
-            Bài viết cùng tác giả
-        </Heading>
-        <Divider />
-        <VStack mt="2">
-            {
-                posts.map(post => (<PostCard
-                    key={post._id}
-                    post={post} />))
-            }
-        </VStack>
+			<Heading>Bài viết cùng tác giả</Heading>
+			<Divider />
+			<VStack mt="2">
+				{posts.map((post) => (
+					<PostCard key={post._id} post={post} />
+				))}
+			</VStack>
 
-        {
-            loading && <PostSkeletons />
-        }
-
-    </Container>
+			{loading && <PostSkeletons />}
+		</Container>
+	);
 }
